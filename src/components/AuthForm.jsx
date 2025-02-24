cb6e1fb21d9c431b12f45babb50fd7b2280dd868
answq <wnffldksla@gmail.com>
2025-02-24 22:24:11 +0900

    로그인, 회원가입 페이지 구현완료, 로컬스토리지 적용

---
+++ src/components/AuthForm.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const AuthForm = ({ mode, onSubmit }) => {
+  const [formData, setFormData] = useState({
+    id: "",
+    password: "",
+    nickname: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  // 폼 제출 시 onSubmit 호출
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData); 
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="id"
+          value={formData.id}
+          onChange={handleChange}
+          placeholder="아이디"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          placeholder="비밀번호"
+          required
+        />
+        {mode === "signup" && (
+          <input
+            type="text"
+            value={formData.nickname}
+            onChange={handleChange}
+            placeholder="닉네임"
+            required
+          />
+        )}
+
+        <button>{mode === "login" ? "로그인" : "회원가입"}</button>
+      </form>
+    </div>
+  );
+};
+
+export default AuthForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,30 @@
-import { useEffect } from "react";
 import AuthForm from "../components/AuthForm";
 import { login, getUserProfile } from "../api/auth";
 import { Link, useNavigate } from "react-router-dom";
 
 const Login = ({ setUser }) => {
+  const navigate = useNavigate();
 
-
+  //로그인
   const handleLogin = async (formData) => {
     try {
+      const data = await login({
+        id: formData.id,
+        password: formData.password,
+      });
+      localStorage.setItem("accessToken", data.accessToken);
+      if (data) {
+        const userProfile = await getUserProfile(data.accessToken);
 
+        setUser(userProfile);
+
+        navigate("/");
+      } else {
+        alert("로그인 오류");
+      }
     } catch (error) {
       alert("로그인에 실패했습니다. 다시 시도해주세요.");
+      console.log(error.response.data.message);
     }
   };
 
@@ -21,10 +35,7 @@ const Login = ({ setUser }) => {
         <AuthForm mode="login" onSubmit={handleLogin} />
         <div>
           <p>
-            계정이 없으신가요?{" "}
-            <Link to="/signup">
-              회원가입
-            </Link>
+            아직 계정이 없으신가요? <Link to="/signup">회원가입</Link>
           </p>
         </div>
       </div>
